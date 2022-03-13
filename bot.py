496baaedf00162f1fb3fac94a98c1b11b6eb807b
Igor Esposito <68132154+Sposigor@users.noreply.github.com>
2022-03-13 12:50:09 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ Colaborar: (see also: git help workflows)
 """
 
 
-updater = Updater(!process.env.TOKEN)
+updater = Updater(TOKEN)
 
 def git_help(update: Update, context: CallbackContext):
     """ Envia a Messagem no Telegram """
