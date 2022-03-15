4cfc86ddce4cfc635e53896410346de08d230544
Edu Londe <72423464+edulonde@users.noreply.github.com>
2022-03-15 15:24:20 -0300

    criando ajuda docker

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
 
-TODOS_OS_COMANDOS = """
+GIT_HELP = """
 Estes são os comandos Git mais comuns usados em várias situações:
 
 Começando uma área de trabalho: (git help tutorial)
@@ -44,13 +44,77 @@ Colaborar: (see also: git help workflows)
 
 """
 
+DOCKER_HELP = '''
+Comandos de gerenciamento:
+  config      Gerenciar Configurações Docker.
+  container   Gerenciar Container.
+  image       Gerenciar Imagens.
+  network     Gerenciar Redes.
+  node        Gerenciar Nós de Swarm.
+  plugin      Gerenciar Plugins.
+  secret      Gerenciar Docker secrets.
+  service     Gerenciar Services.
+  swarm       Gerenciar Swarm.
+  system      Gerenciar Docker.
+  trust       Gerenciar a veracidade das imagens Docker .
+  volume      Gerenciar Volumes.
+
+Comandos:
+  attach      Anexar fluxos de entrada, saída e erro padrão locais a um contêiner em execução. 
+  build       Construir uma imagem através de um Dockerfile.
+  commit      Criar uma nova imagem das mudanças de um container.
+  cp          Copiar arquivos/pastas entre um container e o sistema de arquivos local.
+  create      Criar novo Container.
+  diff        Inspecionar mudanças para arquivos ou diretórios no sistema de arquivos do Container.
+  events      Pegar eventos do servidor em tempo real.
+  exec        Executar um comando em um container ativo.
+  export      Exportar o sistema de arquivos de um container como um arquivo do tipo tar.
+  history     Mostrar a história de uma imagem.
+  images      Lista Imagens.
+  import      Importar o conteúdo de um trabalho para criar uma imagem de um sistema de arquivos.
+  info        Exibe informação de todo o sistema.
+  inspect     Retornar informações de baixo nível sobre objetos do Docker.
+  kill        Encerrar um ou mais contêineres em execução.
+  load        Carregar uma imagem de um arquivo tipo tar ou STDIN.
+  login       Faça login em um registro do Docker.
+  logout      Faça logout em um registro do Docker.
+  logs        Buscar os logs de um contêiner.
+  pause       Pausar todos os processos em um ou mais contêineres.
+  port        Listar mapeamentos de portas ou um mapeamento específico para o contêiner.
+  ps          Listar containers.
+  pull        Puxar uma imagem ou um repositório de um registro.
+  push        Enviar uma imagem ou um repositório para um registro.
+  rename      Renomear um Container.
+  restart     Restart de um ou mais containers.
+  rm          Remover um ou mais containers.
+  rmi         Remover uma ou mais imagens.
+  run         Executar um comando em um novo Container.
+  save        Salvar uma ou mais imagens para um arquivo tipo tar (STDOUT por default).
+  search      Procurar por imagens no Docker Hub.
+  start       Começar um ou mais containers pausados.
+  stats       Exibir uma transmissão ao vivo das estatísticas de uso de recursos dos containers.
+  stop        Pausa um ou mais containers em execução.
+  tag         Cria uma tag TARGET_IMAGE que se refere ao SOURCE_IMAGE.
+  top         Exibir os processos em execução de um contêiner.
+  unpause     Tirar da pausa todos processos em um ou mais containers.
+  update      Atualizar configuração de um ou mais contêineres.
+  version     Mostrar as informações da versão do Docker.
+  wait        Bloquear até que um ou mais contêineres parem e imprima seus códigos de saída.
+
+Execute 'docker COMMAND --help' para mais informações sobre um comando.
+'''
+
 updater = Updater(os.getenv('TOKEN'))
 
 def git_help(update: Update, context: CallbackContext):
     """ Envia a Messagem no Telegram """
-    context.bot.send_message(chat_id=update.effective_chat.id, parse_mode="Markdown", text=TODOS_OS_COMANDOS)
+    context.bot.send_message(chat_id=update.effective_chat.id, parse_mode="Markdown", text=GIT_HELP)
 
+def docker_help(update: Update, context: CallbackContext):
+    """ Envia a Messagem no Telegram """
+    context.bot.send_message(chat_id=update.effective_chat.id, parse_mode="Markdown", text=DOCKER_HELP)   
 
 updater.dispatcher.add_handler(CommandHandler('git_help', git_help))
+updater.dispatcher.add_handler(CommandHandler('docker_help', docker_help))
 
 updater.start_polling()
